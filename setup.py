6b075cba084ec07491ed5dbec70296e09e830e10
clryan <59152688+clryan@users.noreply.github.com>
2020-11-28 15:24:05 -0600

    Init setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+  
+from setuptools import find_packages, setup
+
+setup(name='pyutils',
+      version='0.1',
+      description='Helper functions for pandas and other python tasks',
+      url='https://github.com/clryan/pyutils',
+      author='Claire Ryan',
+      author_email='ryan.claire29@gmail.com',
+      license='MIT',
+      packages=find_packages(where="src"),
+      package_dir={"": "src"},
+      zip_safe=False)
